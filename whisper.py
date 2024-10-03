b3e4b24b7bc66d608f1ab09fa0d810f95157ef16
Aditya Satyadeep <adityasatyadeep2002@gmail.com>
2024-10-02 21:38:32 -0700

    refactor to expose transcript as web endpoint

--- whisper.py
+++ whisper.py
@@ -3,7 +3,7 @@ import modal
 
 MODEL_DIR = os.path.join(os.path.dirname(__file__), "/model")
 MODEL_NAME = "openai/whisper-large-v3"
-# MODEL_REVISION = "afda370583db9c5359511ed5d989400a6199dfe1"
+MODEL_REVISION = "afda370583db9c5359511ed5d989400a6199dfe1"
 
 image = modal.Image.debian_slim().apt_install("ffmpeg").pip_install(
         "torch",
@@ -25,29 +25,28 @@ app = modal.App(
 
 
 
+# build is run as container is being built
+@modal.build()
+def download_model(self):
+    from huggingface_hub import snapshot_download
+    from transformers.utils import move_cache
+
+    os.makedirs(MODEL_DIR, exist_ok=True)
+
+    snapshot_download(
+        MODEL_NAME,
+        local_dir=MODEL_DIR,
+        ignore_patterns=["*.pt", "*.bin"],  # Using safetensors
+        revision=MODEL_REVISION,
+    )
+    move_cache()
+
 @app.cls(
     gpu="a10g",
     concurrency_limit=10,
     mounts=[modal.Mount.from_local_dir("./audio_samples", remote_path="/root/audio_samples")]
 )
-
 class Model:
-    # build is run as container is being built
-    @modal.build()
-    def download_model(self):
-        from huggingface_hub import snapshot_download
-        from transformers.utils import move_cache
-
-        os.makedirs(MODEL_DIR, exist_ok=True)
-
-        snapshot_download(
-            MODEL_NAME,
-            local_dir=MODEL_DIR,
-            ignore_patterns=["*.pt", "*.bin"],  # Using safetensors
-            # revision=MODEL_REVISION,
-        )
-        move_cache()
-
     # enter is run right when container starts
     @modal.enter()
     def load_model(self):
@@ -73,28 +72,28 @@ class Model:
         )
     @modal.batched(max_batch_size=1, wait_ms=1000)
     def transcribe(self, audio):
-        print(os.listdir("/root/audio_samples"))
-
+        print("Processing: ", audio)
         import time
         start = time.time()
         result = self.pipe(audio)
         end = time.time()
-        print(f"Time taken: {end - start}")
+        print(f"Time taken: {end - start}\n")
         return result
 
 
 @app.function(mounts=[modal.Mount.from_local_dir("./audio_samples", remote_path="/root/audio_samples")])
+@modal.web_endpoint()
 def transcribe_audio():
     whisper = Model()
     print("📣 Transcribing")
-    
-    file_paths = ["/root/audio_samples/MLK_Something_happening.mp3"]
+
+    files = os.listdir("/root/audio_samples")
+    file_paths = ["/root/audio_samples/" + f for f in files]
+
+    responses = []
 
     for x in whisper.transcribe.map(file_paths):
-        yield x
+        print(x["text"])
+        responses.append(x['text'])
 
-@app.local_entrypoint()
-def main():
-    
-    for t in transcribe_audio.remote_gen():
-        print(t)
+    return responses
